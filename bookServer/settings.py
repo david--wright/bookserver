a4c18c04ee31e4e2b2d3621fa6c89676c675cfea
David Wright <discipleofhoid@gmail.com>
2017-11-06 15:38:33 -0700

    Change media and static root paths

--- bookServer/settings.py
+++ bookServer/settings.py
@@ -53,7 +53,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/opt/django/persistent/media/'
+MEDIA_ROOT = '/opt/django/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -64,7 +64,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/opt/django/volatile/static/'
+STATIC_ROOT = '/opt/django/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
